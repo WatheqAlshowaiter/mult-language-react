85f8cbbe9646ab8589173d6eeb6ea620ee884d9e
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2024-09-24 21:59:46 +0300

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,40 +5,27 @@ import { Home } from "./pages/Home";
 import { Suspense, useEffect } from "react";
 import i18next from "i18next";
 import { Provider } from "react-redux";
-import  store  from './store';
+import store from './store';
 
 
 const App = () => {
 
   useEffect(() => {
-    const updateDirection = (lng) => {
-      document.body.dir = i18next.dir(lng);
-    };
-
-    // Set initial direction
-    updateDirection(i18next.language);
-
-    // Listen for language changes
-    i18next.on('languageChanged', updateDirection);
-
-    // Cleanup the event listener on unmount
-    return () => {
-      i18next.off('languageChanged', updateDirection);
-    };
+    document.body.dir = i18next.dir();
   }, []);
 
-	return (
+  return (
     <Provider store={store}>
-		<Suspense fallback="loading">
-			<BrowserRouter>
-				<Menu />
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/about" element={<About />} />
-				</Routes>
-			</BrowserRouter>
-		</Suspense>
+      <Suspense fallback="loading">
+        <BrowserRouter>
+          <Menu />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
     </Provider>
-	);
+  );
 };
 export default App;
